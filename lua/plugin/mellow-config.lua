5022122511d0358cd91b9c1b1da6e11d0dd7ea62
vuminhtrinh2804 <vuminhtrinh2804@gmail.com>
2022-12-06 00:21:05 +0700

    clean up comments

--- lua/plugin/mellow-config.lua
+++ lua/plugin/mellow-config.lua
@@ -11,22 +11,3 @@ vim.g.mellow_bold_functions = true --Make functions bold
 --vim.g.mellow_transparent = false --Disable background color
 
 vim.cmd [[colorscheme mellow]]
-
---vim.api.nvim_set_hl(0, 'DiagnosticVirtualTextError', { fg = "#757581", undercurl = true, bold = true })
---vim.api.nvim_set_hl(0, 'DiagnosticVirtualTextWarn', { fg = "#757581", undercurl = true, bold = true })
---vim.api.nvim_set_hl(0, 'DiagnosticVirtualTextHint', { fg = "#757581", undercurl = true, bold = true })
---vim.api.nvim_set_hl(0, 'DiagnosticVirtualTextInfo', { fg = "#757581", undercurl = true, bold = true })
-----vim.api.nvim_set_hl(0, 'DiagnosticVirtualTextError', { fg = "#ea83a5", italic = true, undercurl = true, bold = true })
-----vim.api.nvim_set_hl(0, 'DiagnosticVirtualTextWarn', { fg = "#e6b99d", italic = true, undercurl = true, bold = true })
-----vim.api.nvim_set_hl(0, 'DiagnosticVirtualTextHint', { fg = "#90b99f", italic = true, undercurl = true, bold = true })
-----vim.api.nvim_set_hl(0, 'DiagnosticVirtualTextInfo', { fg = "#aca1cf", italic = true, undercurl = true, bold = true })
---
---vim.api.nvim_set_hl(0, 'DiagnosticError', { fg = "#ea83a5", bold = true })
---vim.api.nvim_set_hl(0, 'DiagnosticWarn', { fg = "#e6b99d", bold = true })
---vim.api.nvim_set_hl(0, 'DiagnosticHint', { fg = "#90b99f", bold = true })
---vim.api.nvim_set_hl(0, 'DiagnosticInfo', { fg = "#aca1cf", bold = true })
-----
---vim.api.nvim_set_hl(0, 'DiagnosticUnderlineError', { sp = "#ea83a5", undercurl = true })
---vim.api.nvim_set_hl(0, 'DiagnosticUnderlineWarn', { sp = "#e6b99d", undercurl = true })
---vim.api.nvim_set_hl(0, 'DiagnosticUnderlineHint', { sp = "#90b99f", undercurl = true })
---vim.api.nvim_set_hl(0, 'DiagnosticUnderlineInfo', { sp = "#aca1cf", undercurl = true })
